a82eab018a40a03bfd9df114c8f2be98f2573e93
Riashabh <meenarishabh35@gmail.com>
2025-10-11 03:30:36 -0700

    Added error handling(try  except)

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+
+
 load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+if not OPENAI_API_KEY:
+    raise RuntimeError("OPENAI_API_KEY missing — check your .env file")
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
@@ -15,17 +21,22 @@ class TitleRequest(BaseModel):
 
 @app.get("/")
 async def root():
-    return { "message" : "Welcome to HowItEnds API!" }
+return { "message" : "Welcome to HowItEnds API!" }
 
 @app.post("/check-ending")
 async def check_ending(request: TitleRequest):
+try: 
     title = request.title
-    prompt = os.getenv("AI_PROMPT").format(title=title)
+                        prompt = os.getenv("AI_PROMPT").format(title=title)
 
-    response = client.responses.create(
-        model="gpt-4o",
-        input=prompt
-    )
+ try:
+        response = client.responses.create(
+            model="gpt-4o",
+            input=prompt
+        )
+    except Exception as e:
+        #any error like network/auth, timeout etc.
+        raise HTTPException(status_code = 503, detail=f"Open AI error{type(e).__name__}")
 
     ending = response.output_text.strip().lower()
     return {"title":  title, "ending": ending}
